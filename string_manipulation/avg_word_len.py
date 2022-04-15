e3f6d01a93cb6f9198ab62cefa4d44eedc6821cd
Lauras5 <idlaurasalcido@gmail.com>
2022-04-15 00:58:32 -0700

    string manip w/ unit tests

---
+++ string_manipulation/avg_word_len.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+def avg_word_len(sentence):
+    for p in "!?',;.":
+        # remove punctuation marks
+        sentence = sentence.replace(p, '')
+    # create arr of sentence
+    words = sentence.split()
+    # return rounded num --> add all word lengths divide by number of words, 2 decimal points
+    return round(sum(len(word) for word in words) / len(words), 2)
+
+
+# tests
+class Test(unittest.TestCase):
+
+    def test_sentence1(self):
+        print("Testing sentence1 \n")
+        self.assertEqual(3.86, avg_word_len("Nice to meet you Tom from MySpace."))
+
+    def test_sentence2(self):
+        print("Testing float num.\n")
+        self.assertEqual(4.2, avg_word_len("This is another sentence, it won't make sense so? yeah."))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ string_manipulation/reverse_num.py
@@ -0,0 +1,35 @@
+# given an number, return w/ reversed digits
+# int or float: signed, can be positive or negative
+import unittest
+
+
+def reverse_num(x):
+    # first we turn int into a string
+    string = str(x)
+    float_num = isinstance(x, float)
+
+    # check if it's negative
+    if string[0] == '-':
+        return float('-' + string[:0:-1]) if float_num else int('-' + string[:0:-1])
+    return float(string[::-1]) if float_num else int(string[::-1])
+
+
+# tests
+class Test(unittest.TestCase):
+
+    def test_negative(self):
+        print("Testing reverse num\n")
+        self.assertEqual(-132, reverse_num(-231))
+
+    def test_float(self):
+        print("Testing float num.\n")
+        self.assertEqual(1.32, reverse_num(23.1))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# still works if user gives float?
+# do we want decimal in same spot?
+# return as float or int?
+# min or max number?
