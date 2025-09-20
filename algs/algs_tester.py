1c9e5c1c975f3964f638d64a350832c8cbdc1e9d
neurowitchai <neurowitch.ai@gmail.com>
2025-09-19 22:40:18 -0700

    update tester with median sorted arr test cases

--- algs/algs_tester.py
+++ algs/algs_tester.py
@@ -1,6 +1,7 @@
 import unittest
 import fib_recursion
 import two_sum
+from median_sorted_arr import Solution  
 
 
 class TestAlgs(unittest.TestCase):
@@ -14,6 +15,18 @@ class TestAlgs(unittest.TestCase):
 
     def test_two_sum__none(self):
         self.assertIsNone(two_sum.two_sum([1, 2, 3], 2))
+
+    def test_median_sorted_arrays_examples(self):
+        s = Solution()
+        self.assertEqual(s.findMedianSortedArrays([1, 3], [2]), 2.0)
+        self.assertEqual(s.findMedianSortedArrays([1, 2], [3, 4]), 2.5)
+
+    def test_median_sorted_arrays_edge_cases(self):
+        s = Solution()
+        self.assertEqual(s.findMedianSortedArrays([], [1]), 1.0)
+        self.assertEqual(s.findMedianSortedArrays([2], [3]), 2.5)
+        self.assertEqual(s.findMedianSortedArrays([1, 2, 3], [4, 5, 6, 7, 8]), 4.5)
+        self.assertEqual(s.findMedianSortedArrays([-5, -3, -1], [-2, 0, 2]), -1.5)
 
 
 if __name__ == '__main__':
---
+++ algs/median_sorted_arr.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        a, b = nums1, nums2
+        # binary search on smaller number
+        if len(a) > len(b): a, b = b, a
+        m, n = len(a), len(b)
+
+        
+        lo, hi, half = 0, m, (m + n + 1) // 2
+        INF = float('inf')
+
+        while True:
+            i = (lo + hi) // 2
+            j = half - i
+
+            # values around the partition
+            aL = a[i - 1] if i > 0 else -INF    # max of left, a
+            aR = a[i] if i < m else INF         # max of right, a
+            bL = b[j - 1] if j > 0 else -INF    # max of left, b
+            bR = b[j] if j < n else INF         # max of right, b
+
+            # check partition
+            if aL <= bR and bL <= aR:
+                if (m + n) % 2:
+                    return float(max(aL, bL))
+                return (max(aL, bL) + min(aR, bR)) / 2.0
+
+            # if aL is too big, move cut left
+            if aL > bR:
+                hi = i - 1
+            # if bL is too big more right
+            else:
+                lo = i  + 1
